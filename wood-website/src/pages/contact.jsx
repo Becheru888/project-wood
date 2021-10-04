c4edd4b547670dbd968757c44864c067bbcea61f
ioana-web <becheru93@gmail.com>
2021-10-04 10:07:31 +0300

    .

--- wood-website/src/pages/contact.jsx
+++ wood-website/src/pages/contact.jsx
@@ -1,7 +1,15 @@
+import Call from '../assets/call.svg';  
+
 export default function Contact(){
     return (
         <div className='contact'>
-            <h1>Contact</h1>
+             <div style={{display:"flex", alignItems:'center'}}>
+              <h3>Contactează-ne</h3>
+              <img width="50px" style={{margin:'0 0 0 10px'}} src={Call}/>
+            </div>
+            <div>
+              <p></p>
+            </div>
         </div>
     )
 }
